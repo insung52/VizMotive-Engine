320656c8ee2dc84e9783f3def695ce3e54817276
Insung <justheare0502@gmail.com>
2026-01-30 17:00:55 +0900

    add cmake

---
+++ GraphicsBackends/GraphicsDevice_Metal.mm
@@ -0,0 +1,262 @@
+#include "GraphicsDevice_Metal.h"
+
+#ifdef __APPLE__
+#import <Foundation/Foundation.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#endif
+
+namespace vz::graphics
+{
+    // Frame allocator for GPU uploads
+    static GraphicsDevice::GPULinearAllocator frameAllocator;
+
+    GraphicsDevice_Metal::~GraphicsDevice_Metal()
+    {
+#ifdef __APPLE__
+        if (commandQueue) {
+            commandQueue = nil;
+        }
+        if (device) {
+            device = nil;
+        }
+#endif
+    }
+
+    bool GraphicsDevice_Metal::Initialize(ValidationMode validationMode_, GPUPreference preference)
+    {
+#ifdef __APPLE__
+        this->validationMode = validationMode_;
+
+        // Get the default Metal device
+        device = MTLCreateSystemDefaultDevice();
+        if (!device) {
+            NSLog(@"Metal is not supported on this device");
+            return false;
+        }
+
+        // Create command queue
+        commandQueue = [device newCommandQueue];
+        if (!commandQueue) {
+            NSLog(@"Failed to create Metal command queue");
+            return false;
+        }
+
+        // Set device info
+        adapterName = [[device name] UTF8String];
+        driverDescription = "Apple Metal";
+
+        NSLog(@"Metal device initialized: %@", [device name]);
+        return true;
+#else
+        return false;
+#endif
+    }
+
+    // === Stub Implementations ===
+
+    bool GraphicsDevice_Metal::CreateSwapChain(const SwapChainDesc* desc, vz::platform::window_type window, SwapChain* swapchain) const
+    {
+        // TODO: Implement Metal swap chain creation with CAMetalLayer
+        return false;
+    }
+
+    bool GraphicsDevice_Metal::CreateBuffer2(const GPUBufferDesc* desc, const std::function<void(void* dest)>& init_callback, GPUBuffer* buffer, const GPUResource* alias, uint64_t alias_offset) const
+    {
+        // TODO: Implement Metal buffer creation with MTLBuffer
+        return false;
+    }
+
+    void GraphicsDevice_Metal::UploadToBufferRegion(GPUBuffer* buffer, uint64_t offset, const void* data, uint64_t size) const
+    {
+        // TODO: Implement
+    }
+
+    bool GraphicsDevice_Metal::CreateTexture(const TextureDesc* desc, const SubresourceData* initial_data, Texture* texture, const GPUResource* alias, uint64_t alias_offset) const
+    {
+        // TODO: Implement Metal texture creation with MTLTexture
+        return false;
+    }
+
+    bool GraphicsDevice_Metal::CreateShader(ShaderStage stage, const void* shadercode, size_t shadercode_size, Shader* shader) const
+    {
+        // TODO: Implement Metal shader creation with MTLLibrary
+        return false;
+    }
+
+    bool GraphicsDevice_Metal::CreateSampler(const SamplerDesc* desc, Sampler* sampler) const
+    {
+        // TODO: Implement Metal sampler creation with MTLSamplerState
+        return false;
+    }
+
+    bool GraphicsDevice_Metal::CreateQueryHeap(const GPUQueryHeapDesc* desc, GPUQueryHeap* queryheap) const
+    {
+        // TODO: Implement
+        return false;
+    }
+
+    bool GraphicsDevice_Metal::CreatePipelineState(const PipelineStateDesc* desc, PipelineState* pso, const RenderPassInfo* renderpass_info) const
+    {
+        // TODO: Implement Metal pipeline state with MTLRenderPipelineState
+        return false;
+    }
+
+    int GraphicsDevice_Metal::CreateSubresource(Texture* texture, SubresourceType type, uint32_t firstSlice, uint32_t sliceCount, uint32_t firstMip, uint32_t mipCount, const Format* format_change, const ImageAspect* aspect, const Swizzle* swizzle, float min_lod_clamp) const
+    {
+        return -1;
+    }
+
+    int GraphicsDevice_Metal::CreateSubresource(GPUBuffer* buffer, SubresourceType type, uint64_t offset, uint64_t size, const Format* format_change, const uint32_t* structuredbuffer_stride_change) const
+    {
+        return -1;
+    }
+
+    bool GraphicsDevice_Metal::OpenSharedResource(const void* device2, const void* srvDescHeap2, const int descriptorIndex, const Texture* textureShared,
+        uint64_t& gpuDesciptorHandlerPtr, GPUResource& sharedRes, void** backendResPtr)
+    {
+        return false;
+    }
+
+    void GraphicsDevice_Metal::DeleteSubresources(GPUResource* resource)
+    {
+        // TODO: Implement
+    }
+
+    int GraphicsDevice_Metal::GetDescriptorIndex(const GPUResource* resource, SubresourceType type, int subresource) const
+    {
+        return -1;
+    }
+
+    int GraphicsDevice_Metal::GetDescriptorIndex(const Sampler* sampler) const
+    {
+        return -1;
+    }
+
+    CommandList GraphicsDevice_Metal::BeginCommandList(QUEUE_TYPE queue)
+    {
+        // TODO: Implement with MTLCommandBuffer
+        return CommandList{};
+    }
+
+    void GraphicsDevice_Metal::SubmitCommandLists()
+    {
+        // TODO: Implement
+        FRAMECOUNT++;
+    }
+
+    void GraphicsDevice_Metal::WaitForGPU() const
+    {
+        // TODO: Implement with waitUntilCompleted
+    }
+
+    void GraphicsDevice_Metal::ClearPipelineStateCache()
+    {
+        // TODO: Implement
+    }
+
+    size_t GraphicsDevice_Metal::GetActivePipelineCount() const
+    {
+        return 0;
+    }
+
+    ShaderFormat GraphicsDevice_Metal::GetShaderFormat() const
+    {
+        return ShaderFormat::HLSL6; // TODO: Change to Metal shader format when supported
+    }
+
+    Texture GraphicsDevice_Metal::GetBackBuffer(const SwapChain* swapchain) const
+    {
+        return Texture{};
+    }
+
+    ColorSpace GraphicsDevice_Metal::GetSwapChainColorSpace(const SwapChain* swapchain) const
+    {
+        return ColorSpace::SRGB;
+    }
+
+    bool GraphicsDevice_Metal::IsSwapChainSupportsHDR(const SwapChain* swapchain) const
+    {
+        return false;
+    }
+
+    uint32_t GraphicsDevice_Metal::GetMinOffsetAlignment(const GPUBufferDesc* desc) const
+    {
+        return 256; // Metal's typical alignment requirement
+    }
+
+    GraphicsDevice::MemoryUsage GraphicsDevice_Metal::GetMemoryUsage() const
+    {
+        MemoryUsage usage{};
+#ifdef __APPLE__
+        if (device) {
+            // Metal doesn't directly expose memory usage, but we can query recommended working set
+            if (@available(macOS 10.13, *)) {
+                usage.budget = [device recommendedMaxWorkingSetSize];
+            }
+        }
+#endif
+        return usage;
+    }
+
+    uint32_t GraphicsDevice_Metal::GetMaxViewportCount() const
+    {
+        return 16; // Metal supports multiple viewports
+    }
+
+    void GraphicsDevice_Metal::WaitCommandList(CommandList cmd, CommandList wait_for) {}
+    void GraphicsDevice_Metal::RenderPassBegin(const SwapChain* swapchain, CommandList cmd) {}
+    void GraphicsDevice_Metal::RenderPassBegin(const RenderPassImage* images, uint32_t image_count, CommandList cmd, RenderPassFlags flags) {}
+    void GraphicsDevice_Metal::RenderPassEnd(CommandList cmd) {}
+    void GraphicsDevice_Metal::BindScissorRects(uint32_t numRects, const Rect* rects, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindViewports(uint32_t NumViewports, const Viewport* pViewports, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindResource(const GPUResource* resource, uint32_t slot, CommandList cmd, int subresource) {}
+    void GraphicsDevice_Metal::BindResources(const GPUResource* const* resources, uint32_t slot, uint32_t count, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindUAV(const GPUResource* resource, uint32_t slot, CommandList cmd, int subresource) {}
+    void GraphicsDevice_Metal::BindUAVs(const GPUResource* const* resources, uint32_t slot, uint32_t count, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindSampler(const Sampler* sampler, uint32_t slot, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindConstantBuffer(const GPUBuffer* buffer, uint32_t slot, CommandList cmd, uint64_t offset) {}
+    void GraphicsDevice_Metal::BindVertexBuffers(const GPUBuffer* const* vertexBuffers, uint32_t slot, uint32_t count, const uint32_t* strides, const uint64_t* offsets, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindIndexBuffer(const GPUBuffer* indexBuffer, const IndexBufferFormat format, uint64_t offset, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindStencilRef(uint32_t value, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindBlendFactor(float r, float g, float b, float a, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindPipelineState(const PipelineState* pso, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindComputeShader(const Shader* cs, CommandList cmd) {}
+    void GraphicsDevice_Metal::BindDepthBounds(float min_bounds, float max_bounds, CommandList cmd) {}
+    void GraphicsDevice_Metal::Draw(uint32_t vertexCount, uint32_t startVertexLocation, CommandList cmd) {}
+    void GraphicsDevice_Metal::DrawIndexed(uint32_t indexCount, uint32_t startIndexLocation, int32_t baseVertexLocation, CommandList cmd) {}
+    void GraphicsDevice_Metal::DrawInstanced(uint32_t vertexCount, uint32_t instanceCount, uint32_t startVertexLocation, uint32_t startInstanceLocation, CommandList cmd) {}
+    void GraphicsDevice_Metal::DrawIndexedInstanced(uint32_t indexCount, uint32_t instanceCount, uint32_t startIndexLocation, int32_t baseVertexLocation, uint32_t startInstanceLocation, CommandList cmd) {}
+    void GraphicsDevice_Metal::DrawInstancedIndirect(const GPUBuffer* args, uint64_t args_offset, CommandList cmd) {}
+    void GraphicsDevice_Metal::DrawIndexedInstancedIndirect(const GPUBuffer* args, uint64_t args_offset, CommandList cmd) {}
+    void GraphicsDevice_Metal::DrawInstancedIndirectCount(const GPUBuffer* args, uint64_t args_offset, const GPUBuffer* count, uint64_t count_offset, uint32_t max_count, CommandList cmd) {}
+    void GraphicsDevice_Metal::DrawIndexedInstancedIndirectCount(const GPUBuffer* args, uint64_t args_offset, const GPUBuffer* count, uint64_t count_offset, uint32_t max_count, CommandList cmd) {}
+    void GraphicsDevice_Metal::Dispatch(uint32_t threadGroupCountX, uint32_t threadGroupCountY, uint32_t threadGroupCountZ, CommandList cmd) {}
+    void GraphicsDevice_Metal::DispatchIndirect(const GPUBuffer* args, uint64_t args_offset, CommandList cmd) {}
+    void GraphicsDevice_Metal::CopyResource(const GPUResource* pDst, const GPUResource* pSrc, CommandList cmd) {}
+    void GraphicsDevice_Metal::CopyBuffer(const GPUBuffer* pDst, uint64_t dst_offset, const GPUBuffer* pSrc, uint64_t src_offset, uint64_t size, CommandList cmd) {}
+    void GraphicsDevice_Metal::CopyTexture(const Texture* dst, uint32_t dstX, uint32_t dstY, uint32_t dstZ, uint32_t dstMip, uint32_t dstSlice, const Texture* src, uint32_t srcMip, uint32_t srcSlice, CommandList cmd, const Box* srcbox, ImageAspect dst_aspect, ImageAspect src_aspect) {}
+    void GraphicsDevice_Metal::QueryBegin(const GPUQueryHeap* heap, uint32_t index, CommandList cmd) {}
+    void GraphicsDevice_Metal::QueryEnd(const GPUQueryHeap* heap, uint32_t index, CommandList cmd) {}
+    void GraphicsDevice_Metal::QueryResolve(const GPUQueryHeap* heap, uint32_t index, uint32_t count, const GPUBuffer* dest, uint64_t dest_offset, CommandList cmd) {}
+    void GraphicsDevice_Metal::Barrier(const GPUBarrier* barriers, uint32_t numBarriers, CommandList cmd) {}
+    void GraphicsDevice_Metal::PushConstants(const void* data, uint32_t size, CommandList cmd, uint32_t offset) {}
+    void GraphicsDevice_Metal::ClearUAV(const GPUResource* resource, uint32_t value, CommandList cmd) {}
+
+    void GraphicsDevice_Metal::EventBegin(const char* name, CommandList cmd) {}
+    void GraphicsDevice_Metal::EventEnd(CommandList cmd) {}
+    void GraphicsDevice_Metal::SetMarker(const char* name, CommandList cmd) {}
+
+    RenderPassInfo GraphicsDevice_Metal::GetRenderPassInfo(CommandList cmd)
+    {
+        return RenderPassInfo{};
+    }
+
+    void GraphicsDevice_Metal::Map(GPUResource* resource) {}
+    void GraphicsDevice_Metal::Unmap(GPUResource* resource) {}
+
+    GraphicsDevice::GPULinearAllocator& GraphicsDevice_Metal::GetFrameAllocator(CommandList cmd)
+    {
+        return frameAllocator;
+    }
+}
